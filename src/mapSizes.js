f41ce9e8aab5f083583d0092799230bebc762b59
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-05-27 11:01:10 +0300

    map sizes and world

---
+++ src/mapSizes.js
@@ -0,0 +1,5 @@
+export const MAP_SIZES = {
+  small: { width: 64, height: 32 },
+  medium: { width: 128, height: 64 },
+  large: { width: 256, height: 128 },
+}
---
+++ src/world.js
@@ -0,0 +1,10 @@
+import { MAP_SIZES } from './mapSizes.js'
+
+export const world = {
+  width: MAP_SIZES.small.width,
+  height: MAP_SIZES.small.height,
+
+  tileSize: 32,
+
+  tiles: [],
+}
