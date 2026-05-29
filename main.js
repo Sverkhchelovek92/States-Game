67631f1d0281730e2d1aa67ba2e3d5284a73c1ab
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-05-29 10:48:43 +0300

    array of tiles

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 import { engine } from './src/engine.js'
 
+import { world } from './src/world.js'
+
+world.generate()
+
 engine.init()
--- src/world.js
+++ src/world.js
@@ -1,5 +1,7 @@
 import { MAP_SIZES } from './mapSizes.js'
 
+const currentMapSize = MAP_SIZES.small
+
 export const world = {
   width: MAP_SIZES.small.width,
   height: MAP_SIZES.small.height,
@@ -7,4 +9,38 @@ export const world = {
   tileSize: 32,
 
   tiles: [],
+
+  generate() {
+    this.tiles = []
+
+    for (let y = 0; y < this.height; y++) {
+      const row = []
+
+      for (let x = 0; x < this.width; x++) {
+        // temporary random terrain
+        const random = Math.random()
+
+        let type = 'grass'
+
+        if (random < 0.15) {
+          type = 'water'
+        }
+
+        if (random > 0.85) {
+          type = 'forest'
+        }
+
+        row.push({
+          x,
+          y,
+          type,
+        })
+      }
+
+      this.tiles.push(row)
+    }
+
+    console.log('World generated')
+    console.log(this.tiles)
+  },
 }
