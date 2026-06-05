2b1ffb00f1a8533805a357dc178fbeccfdc296d8
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-05 11:02:27 +0300

    update hover func

--- src/input/hover.js
+++ src/input/hover.js
@@ -1,5 +1,29 @@
+import { camera } from '../camera.js'
+import { world } from '../world.js'
+
 export const hover = {
   tileX: 0,
   tileY: 0,
   tile: null,
+}
+
+export function updateHover(mouseX, mouseY) {
+  const worldX = mouseX + camera.x
+  const worldY = mouseY + camera.y
+
+  let tileX = Math.floor(worldX / world.tileSize)
+
+  const tileY = Math.floor(worldY / world.tileSize)
+
+  tileX = ((tileX % world.width) + world.width) % world.width
+
+  if (tileY < 0 || tileY >= world.height) {
+    hover.tile = null
+    return
+  }
+
+  hover.tileX = tileX
+  hover.tileY = tileY
+
+  hover.tile = world.tiles[tileY][tileX]
 }
