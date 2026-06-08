4cece203274e423f684fe249e78198dc8a4d9d94
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-08 11:35:17 +0300

    select hovered tile

--- src/input/selection.js
+++ src/input/selection.js
@@ -1,5 +1,17 @@
+import { hover } from './hover.js'
+
 export const selection = {
   tileX: null,
   tileY: null,
   tile: null,
+}
+
+export function selectHoveredTile() {
+  if (!hover.tile) {
+    return
+  }
+
+  selection.tileX = hover.tileX
+  selection.tileY = hover.tileY
+  selection.tile = hover.tile
 }
