0c11bc44d054811652ca64d510d117d411021c3e
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-13 10:47:43 +0300

    select unit

--- src/units/unitSelection.js
+++ src/units/unitSelection.js
@@ -1,3 +1,12 @@
+import { hover } from '../input/hover.js'
+import { getUnitAt } from './unitUtils.js'
+
 export const unitSelection = {
   unit: null,
+}
+
+export function selectUnitUnderCursor() {
+  const unit = getUnitAt(hover.tileX, hover.tileY)
+
+  unitSelection.unit = unit
 }
