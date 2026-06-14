9a8ca007d34ef46c00585c3bf54b0cf6d60e39ed
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-14 13:17:55 +0300

    unit actions func

---
+++ src/units/unitActions.js
@@ -0,0 +1,13 @@
+import { unitSelection } from './unitSelection.js'
+import { hover } from '../input/hover.js'
+
+export function moveSelectedUnit() {
+  const unit = unitSelection.unit
+
+  if (!unit) {
+    return
+  }
+
+  unit.x = hover.tileX
+  unit.y = hover.tileY
+}
