98ad089e73bdd9161c9c0607edf8f98a5cddecbd
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-20 10:33:50 +0300

    unit types

---
+++ src/units/unitTypes.js
@@ -0,0 +1,21 @@
+export const UNIT_TYPES = {
+  settler: {
+    name: 'Settler',
+
+    movement: 1,
+    vision: 2,
+
+    attack: 0,
+    defense: 1,
+  },
+
+  warrior: {
+    name: 'Warrior',
+
+    movement: 1,
+    vision: 2,
+
+    attack: 1,
+    defense: 1,
+  },
+}
