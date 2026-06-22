cb5cf6b5bec77857da1ea6910396a167c32ab1fa
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-22 10:25:04 +0300

    new endturn func

--- src/game/turnSystem.js
+++ src/game/turnSystem.js
@@ -1,7 +1,12 @@
 import { game } from './game.js'
+import { world } from '../world.js'
 
 export function endTurn() {
   game.turn++
+
+  for (const unit of world.units) {
+    unit.movement = unit.maxMovement
+  }
 
   console.log(`Turn ${game.turn}`)
 }
