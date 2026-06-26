5e6b865a8b6a722b66f3802823614e948e4cae8c
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-26 10:02:14 +0300

    unit factory owner

--- src/units/unitFactory.js
+++ src/units/unitFactory.js
@@ -2,7 +2,7 @@ import { UNIT_TYPES } from './unitTypes.js'
 
 let nextUnitId = 1
 
-export function createUnit(type, x, y) {
+export function createUnit(type, x, y, owner) {
   const unitType = UNIT_TYPES[type]
 
   if (!unitType) {
@@ -11,6 +11,8 @@ export function createUnit(type, x, y) {
 
   return {
     id: nextUnitId++,
+
+    owner,
 
     type,
 
