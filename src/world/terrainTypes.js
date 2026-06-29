4aa9a8392fe37e6052c3a7e6cdc61ee76b2b12d5
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-06-29 08:33:40 +0300

    terrain types

---
+++ src/world/terrainTypes.js
@@ -0,0 +1,31 @@
+export const TERRAIN_TYPES = {
+  grass: {
+    name: 'Grassland',
+
+    color: '#4caf50',
+
+    movementCost: 1,
+
+    passable: true,
+  },
+
+  forest: {
+    name: 'Forest',
+
+    color: '#2e7d32',
+
+    movementCost: 2,
+
+    passable: true,
+  },
+
+  water: {
+    name: 'Water',
+
+    color: '#2196f3',
+
+    movementCost: 999,
+
+    passable: false,
+  },
+}
