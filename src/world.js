c8752fd1da6b372682a4295d203df6305e77a639
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-07-11 11:29:17 +0300

    generate terrain func

--- src/world.js
+++ src/world.js
@@ -26,6 +26,46 @@ export const world = {
     return y / (this.height - 1)
   },
 
+  generateTerrain() {
+    this.tiles = []
+
+    for (let y = 0; y < this.height; y++) {
+      const row = []
+
+      for (let x = 0; x < this.width; x++) {
+        // latitude
+        const latitude = this.getLatitude(y)
+
+        const distanceFromEquator = Math.abs(latitude - 0.5) * 2
+
+        // temporary random terrain
+        const random = Math.random()
+
+        let type = 'grass'
+
+        if (distanceFromEquator > 0.9) {
+          type = 'snow'
+        }
+
+        if (random < 0.15) {
+          type = 'water'
+        }
+
+        if (random > 0.85) {
+          type = 'forest'
+        }
+
+        row.push({
+          x,
+          y,
+          type,
+        })
+      }
+
+      this.tiles.push(row)
+    }
+  },
+
   generate() {
     this.tiles = []
     this.units = []
