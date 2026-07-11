6a4ddc97dee529f8dac0af6eb4f81f65a6b99a07
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-07-11 11:31:00 +0300

    new generate func

--- src/world.js
+++ src/world.js
@@ -77,50 +77,9 @@ export const world = {
   },
 
   generate() {
-    this.tiles = []
-    this.units = []
+    this.generateTerrain()
 
-    for (let y = 0; y < this.height; y++) {
-      const row = []
-
-      for (let x = 0; x < this.width; x++) {
-        // latitude
-        const latitude = this.getLatitude(y)
-
-        const distanceFromEquator = Math.abs(latitude - 0.5) * 2
-
-        // temporary random terrain
-        const random = Math.random()
-
-        let type = 'grass'
-
-        if (distanceFromEquator > 0.9) {
-          type = 'snow'
-        }
-
-        if (random < 0.15) {
-          type = 'water'
-        }
-
-        if (random > 0.85) {
-          type = 'forest'
-        }
-
-        row.push({
-          x,
-          y,
-          type,
-        })
-      }
-
-      this.tiles.push(row)
-    }
-
-    this.units.push(createUnit('settler', 10, 10, 1))
-
-    this.units.push(createUnit('warrior', 11, 10, 1))
-
-    this.units.push(createUnit('warrior', 13, 10, 1))
+    this.generateUnits()
 
     console.log('World generated')
     console.log(this.tiles)
