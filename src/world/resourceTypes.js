8a9b95fc292a9b1f873ee629b5544a55f88b41dc
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-07-31 11:56:26 +0300

    res types

---
+++ src/world/resourceTypes.js
@@ -0,0 +1,51 @@
+export const RESOURCE_TYPES = {
+  wheat: {
+    name: 'Wheat',
+
+    symbol: '🌾',
+
+    terrains: ['grass'],
+
+    elevations: ['flat', 'hill'],
+  },
+
+  horse: {
+    name: 'Horse',
+
+    symbol: '🐎',
+
+    terrains: ['grass'],
+
+    elevations: ['flat'],
+  },
+
+  iron: {
+    name: 'Iron',
+
+    symbol: '⛏',
+
+    terrains: ['grass', 'tundra', 'desert'],
+
+    elevations: ['hill', 'mountain'],
+  },
+
+  fish: {
+    name: 'Fish',
+
+    symbol: '🐟',
+
+    terrains: ['water'],
+
+    elevations: ['flat'],
+  },
+
+  gold: {
+    name: 'Gold',
+
+    symbol: '💰',
+
+    terrains: ['desert', 'hill'],
+
+    elevations: ['hill', 'mountain'],
+  },
+}
