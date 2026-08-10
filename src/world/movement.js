5cf965031e6c2f70f12e6e845dd69cc5014bfb9f
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-08-10 12:10:13 +0300

    move cost

---
+++ src/world/movement.js
@@ -0,0 +1,21 @@
+import { TERRAIN_TYPES } from './terrainTypes.js'
+
+export function getMovementCost(tile) {
+  const terrain = TERRAIN_TYPES[tile.terrain]
+
+  if (!terrain) {
+    return Infinity
+  }
+
+  let cost = terrain.movementCost
+
+  if (tile.elevation === 'hill') {
+    cost += 1
+  }
+
+  if (tile.elevation === 'mountain') {
+    return Infinity
+  }
+
+  return cost
+}
