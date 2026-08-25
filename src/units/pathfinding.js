9292914c2b563720949fca5ad60f8f30e53ebcec
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-08-25 12:26:29 +0300

    get lowest cost node

--- src/units/pathfinding.js
+++ src/units/pathfinding.js
@@ -2,3 +2,10 @@ import { world } from '../world.js'
 import { getNeighbors } from '../world/neighbors.js'
 import { getMovementCost } from '../world/movement.js'
 import { getUnitAt } from './unitUtils.js'
+
+export function findPath(unit, targetX, targetY) {
+  const startX = unit.x
+  const startY = unit.y
+
+  const targetTile = world.tiles[targetY][targetX]
+}
