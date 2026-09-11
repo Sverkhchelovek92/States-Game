cacbc2422de489d46df2b5e245fd032bb51dd503
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-09-11 10:49:33 +0300

    check distance

--- src/units/unitCombat.js
+++ src/units/unitCombat.js
@@ -1,8 +1,27 @@
+import { world } from '../world.js'
+
 export function attackUnit(attacker, target) {
+  const distanceX = getWrappedDistanceX(attacker.x, target.x, world.width)
+
+  const distanceY = Math.abs(attacker.y - target.y)
+
+  const distance = Math.max(distanceX, distanceY)
+
+  if (distance > 1) {
+    console.log('Target is too far away')
+    return
+  }
+
   target.health -= attacker.attack
 
   console.log(
     `${attacker.type} attacked ${target.type}.`,
     `Target health: ${target.health}`,
   )
+}
+
+function getWrappedDistanceX(x1, x2, worldWidth) {
+  const distance = Math.abs(x1 - x2)
+
+  return Math.min(distance, worldWidth - distance)
 }
