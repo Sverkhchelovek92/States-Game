3e30adff388d0a30abafc3e0bddbd13181efd4a3
Miroslav Gurnyak <mirmiroslav@gmail.com>
2026-09-15 11:24:30 +0300

    new unit types

--- src/units/unitCombat.js
+++ src/units/unitCombat.js
@@ -17,6 +17,10 @@ export function attackUnit(attacker, target) {
     return
   }
 
+  const damageToTarget = Math.max(1, attacker.attack - target.defense / 2)
+
+  const damageToAttacker = Math.max(1, target.attack / 4)
+
   target.health -= attacker.attack
 
   attacker.movement = 0
--- src/units/unitTypes.js
+++ src/units/unitTypes.js
@@ -21,9 +21,37 @@ export const UNIT_TYPES = {
     movement: 2,
     vision: 2,
 
-    attack: 1,
-    defense: 1,
+    attack: 20,
+    defense: 10,
 
     maxHealth: 100,
+  },
+
+  archer: {
+    name: 'Archer',
+
+    color: '#8e44ad',
+
+    movement: 2,
+    vision: 2,
+
+    attack: 15,
+    defense: 5,
+
+    maxHealth: 80,
+  },
+
+  scout: {
+    name: 'Scout',
+
+    color: '#2980b9',
+
+    movement: 3,
+    vision: 4,
+
+    attack: 10,
+    defense: 5,
+
+    maxHealth: 80,
   },
 }
